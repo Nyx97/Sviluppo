fd731fc73a5f67e4773662c3e6dab7c6a2470700
Nyx97 <nardi.nicol8@gmail.com>
2023-12-06 18:39:22 +0100

    nuovo commit

--- prova_1/script.js
+++ prova_1/script.js
@@ -9,12 +9,20 @@ function dropd() {
 
 function openClose() {
   let openPath = document.getElementById("open-path");
-  let close = document.getElementById("close");
   if (openPath.getAttribute("d") === "M4 6h16M4 12h16M4 18h16") {
     openPath.setAttribute("d", "M6 18L18 6M6 6l12 12");
-    close.classList.remove("hidden");
+    menu.classList.remove("hidden");
   } else {
     openPath.setAttribute("d", "M4 6h16M4 12h16M4 18h16");
-    close.classList.add("hidden");
+    menu.classList.add("hidden");
+  }
+}
+
+function toggleDropdown() {
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  if (dropdownMenu.style.display === "none") {
+    dropdownMenu.style.display = "block";
+  } else {
+    dropdownMenu.style.display = "none";
   }
 }
